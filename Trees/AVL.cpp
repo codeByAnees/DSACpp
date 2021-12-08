f88e03537be18a6c80f892c6c7637985d4100c42
aneese421@gmail.com <aneese421@gmail.com>
2021-12-09 01:30:25 +0500

    init

--- Trees/AVL.cpp
+++ Trees/AVL.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include <queue>
+using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;
@@ -10,6 +12,76 @@ struct BST {
 };
 
 BST *root = NULL, *current = NULL;
+
+
+// int count = 0, index = -1;
+// int size = 30;
+// int arr[30] = {};
+
+// int leftSubTree(BST *p) {
+//     if (p != NULL) {
+//         count++;
+//         leftSubTree(p -> left);
+//         arr[++index] = count;
+//         count--;
+//         leftSubTree(p -> right);
+//     }
+//     int largest = arr[0];
+//     for (int i = 0; i < size; i++) {
+//         if (arr[i] > largest) {
+//             largest = arr[i];
+//         }
+//     }
+//     // count = 0, index = -1;
+//     // size = 30;
+//     // arr[30] = {};
+//     return largest;
+// }
+
+// int count1 = 0, index1 = -1;
+// int arr1[30] = {};
+
+// int rightSubTree(BST *q) {
+//     if (q != NULL) {
+//         count1++;
+//         rightSubTree(q -> right);
+//         arr1[++index1] = count1;
+//         count1--;
+//         if (q -> left == NULL) {
+//             count1--;
+//         }
+//         rightSubTree(q -> left);
+//     }
+//     int large = arr1[0];
+//     for (int i = 0; i < size; i++) {
+//         if (arr1[i] > large) {
+//             large = arr1[i];
+//         }
+//     }
+//     // count1 = 0, index1 = -1;
+//     // arr1[30] = {};
+//     return large;
+// }
+
+// void balencing() {
+//     BST *p = root;
+//     BST *q = root -> left;
+//     int leftH = leftSubTree(q -> left);
+//     int rightH = leftSubTree(q -> right);
+//     q -> bf = leftH - rightH;
+//     cout << "Left: " << leftH << endl;
+//     cout << "Right: " << rightH << endl;
+//     cout << "Balencing: " << q -> bf << endl;
+// }
+
+int height(BST *p) {
+    if (p == NULL) {
+        return -1;
+    }
+    int Lheight = height(p -> left);
+    int Rheight = height(p -> right);
+    return max(Lheight, Rheight) + 1;
+}
 
 void insert() {
     current = new BST;
@@ -34,55 +106,35 @@ void insert() {
     }
 }
 
-int count = 0, index = -1;
-int size = 30;
-int *arr;
-arr = new int[size];
-
-int leftSubTree(BST *p) {
+void PreOrder(BST *p) {
     if (p != NULL) {
-        count++;
-        leftSubTree(p -> left);
-        arr[++index] = count;
-        count--;
-        leftSubTree(p -> right);
+        cout << p -> id << endl;
+        PreOrder(p -> left);
+        PreOrder(p -> right);
     }
-    int largest = arr[0];
-    for (int i = 0; i < size; i++) {
-        if (arr[i] > largest) {
-            largest = arr[i];
+}
+
+int main() {
+    int x;
+    do {
+        cout << "\n\t\tA V L\n";
+        cout << "\nEnter 1 to insert \nEnter 2 to display \nEnter 3 for height "
+                "\nEnter 0 to QUIT--> ";
+        cin >> x;
+        switch (x) {
+            case 0:
+                break;
+            case 1:
+                insert();
+                cout << "\nSuccessful!\n";
+                break;
+            case 2:
+                PreOrder(root);
+                break;
+            case 3:
+                cout << "Height: " << height(root);
+                break;
         }
-    }
-    return largest;
+    } while(x != 0);
 }
 
-int count1 = 0, index1 = -1;
-int *arr1;
-arr1 = new int[size];
-
-int rightSubTree(BST *q) {
-    if (q != NULL) {
-        count1++;
-        rightSubTree(q -> right);
-        arr1[++index1] = count1;
-        count1--;
-        rightSubTree(q -> left);
-    }
-    int large = arr1[0];
-    for (int i = 0; i < size; i++) {
-        if (arr1[i] > large) {
-            large = arr1[i];
-        }
-    }
-    return large;
-}
-
-void balencing(BST *k) {
-    if (k != NULL) {
-        balencing(k -> left);
-        int leftH = leftSubTree(k -> left);
-        int rightH = rightSubTree(k -> right);
-        k -> bf = leftH - rightH;
-        balencing(k -> right);
-    }
-}
