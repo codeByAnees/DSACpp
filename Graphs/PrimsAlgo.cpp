9aa14742484299ce438c8cfe2c31d620a0b6dda2
aneese421@gmail.com <aneese421@gmail.com>
2021-12-13 20:31:46 +0500

    init

--- Graphs/PrimsAlgo.cpp
+++ Graphs/PrimsAlgo.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 void prims(int graph[][5], int n) {
     int distance[n];
+    int parent[n];
     bool visited[n];
     for (int i = 0; i < n; i++) {
         distance[i] = INT_MAX;
+        parent[i] = INT_MAX;
         visited[i] = false;
     }
     distance[0] = 0;
@@ -20,13 +22,17 @@ void prims(int graph[][5], int n) {
         visited[min] = true;
         for (int k = 0; k < n; k++) {
             if (graph[min][k] != 0 && (graph[min][k]) < distance[k]) {
-                distance[k] = graph[min][k];
+                if (!visited[k]) {
+                    distance[k] = graph[min][k];
+                    parent[k] = min;
+                }
             }
         }
     }
-    cout << "Vertex     Distance from source" << endl;
-    for (int i = 0; i < n; i++) {
-        cout << i << "              " << distance[i] << endl;
+    // cout << "Vertex     Distance from source" << endl;
+    cout<<"Edge \tWeight\n";
+    for (int i = 1; i < n; i++) {
+        cout << parent[i] << " - " << i << " \t" << distance[i] << endl;
     }
 }
 
@@ -67,10 +73,35 @@ int main() {
     //     {0 ,0, 0, 9, 0}
     // };
 
-    int arr[5][5] = { { 0, 2, 0, 6, 0 },
-                        { 2, 0, 3, 8, 5 },
-                        { 0, 3, 0, 0, 7 },
-                        { 6, 8, 0, 0, 9 },
-                        { 0, 5, 7, 9, 0 } };
+    // int arr[5][5] = { { 0, 2, 0, 6, 0 },
+    //                     { 2, 0, 3, 8, 5 },
+    //                     { 0, 3, 0, 0, 7 },
+    //                     { 6, 8, 0, 0, 9 },
+    //                     { 0, 5, 7, 9, 0 } };
+    // int arr[8][8] = {
+    //     {0,0,3,0,25,18,2,0},
+    //     {0,0,0,2,7,0,0,3},
+    //     {0,4,0,3,0,3,0,0},
+    //     {10,7,3,18,0,0,0,0},
+    //     {0,10,0,25,0,0,7,0},
+    //     {4,9,0,0,0,0,3,0},
+    //     {0,8,0,0,0,10,0,4},
+    //     {8,0,4,0,10,7,0,9}
+    // };
+    // int arr[6][6] = {
+    //     {0,16,0,0,19,21},
+    //     {16,0,6,5,0,11},
+    //     {0,6,0,10,0,0},
+    //     {0,5,10,0,18,14},
+    //     {19,0,0,18,0,33},
+    //     {21,11,0,14,33,0}
+    // };
+    int arr[5][5] = {
+        { 0, 2, 0, 6, 0 },
+        { 2, 0, 3, 8, 5 },
+        { 0, 3, 0, 0, 7 },
+        { 6, 8, 0, 0, 9 },
+        { 0, 5, 7, 9, 0 },
+    };
     prims(arr, 5);
 }
